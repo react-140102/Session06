a3496dc94c8ca4cee714f33626964702df009607
pashaie <pashaie@users.noreply.github.com>
2022-05-24 20:25:04 +0430

    useref

--- src/components/SelectColor.tsx
+++ src/components/SelectColor.tsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export function SelectColor() {
-  const [color, setColor] = useState("red");
+  // const [color, setColor] = useState("red");
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const setSelectColor = (val: string) => {
+    if (selectRef && selectRef.current) {
+      selectRef.current.style.color = val;
+    }
+  };
   return (
     <>
-      <select onChange={(e) => setColor(e.target.value)}>
+      {/* <select
+        style={{ color }}
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      > */}
+      <select ref={selectRef} onChange={(e) => setSelectColor(e.target.value)}>
         <option value="red">Red</option>
         <option value="green">Green</option>
         <option value="blue">Blue</option>
